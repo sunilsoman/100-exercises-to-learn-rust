88068e137f980166ef66f0f2b25f41d42bfc696f
sunilsoman <sunilmathewsoman@gmail.com>
2025-09-30 23:38:54 -0700

    rust exercises 1,2

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut fact: u32 = 1;
+    let mut m: u32 = n;
+
+    while m >= 1 {
+        fact *= m;
+        m -= 1;
+    }
+
+    fact
 }
 
 #[cfg(test)]
